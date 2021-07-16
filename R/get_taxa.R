506d445752f1af1e7c5d29c39a3bb481f2a33ef8
ppipek <ppipek@gmail.com>
2021-07-16 16:50:42 +0200

    Update get_taxa.R

--- R/get_taxa.R
+++ R/get_taxa.R
@@ -4,16 +4,14 @@
 #'
 #' @param scientific A string pattern to filter scientific names
 #' @param common A string pattern to filter common names
-#' @importFrom urltools param_set
-#' @importFrom utils URLencode
+#' @importFrom urltools param_set url_encode
 #' @export
-get_taxa <- function(scientific = "", common = ""){
+get_taxa <- function(access_token, scientific = "", common = ""){
   url <- "https://avisample.net/api/taxon"
   url <- urltools::param_set(url, key = "filter[name][like]", value = utils::URLencode(common))
   url <- urltools::param_set(url, key = "filter[latin][like]", value = utils::URLencode(scientific))
   # url <- param_set(url, key = "filter[family][like]", value = URLencode(country))
   # url <- param_set(url, key = "per-page", value = perPage)
   # url <- param_set(url, key = "limit", value = limit)
-
-  return(get_items(url))
+  return(get_items(url, access_token))
 }
