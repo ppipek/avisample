5a5eedf1bbbebf6945d9cf5710700412e34c51eb
ppipek <ppipek@gmail.com>
2021-07-16 15:00:11 +0200

    Update get_tissue_types.R

--- R/get_tissue_types.R
+++ R/get_tissue_types.R
@@ -1,13 +1,15 @@
 #' Get Tissue Types
 #'
-#' Internal function that retrieves list of all taxa matching a filter string
+#' @description Internal function that retrieves list of all taxa matching a filter string
 #'
 #' @param name A string pattern to filter tissue type names
 #' @importFrom jsonlite fromJSON
+#' @importFrom urltools param_set
+#' @importFrom utils URLencode
 #' @export
 get_tissue_types <- function (name = NULL){
   url <- "https://avisample.net/api/tissue"
-  url <- urltools::param_set(url, key = "filter[name][like]", value = urltools::URLencode(name))
+  url <- urltools::param_set(url, key = "filter[name][like]", value = utils::URLencode(name))
 
   return(get_items(url))
 }
