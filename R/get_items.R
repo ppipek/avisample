770aa9eb6123336f05e07c00566a8a04cf9a1747
ppipek <ppipek@gmail.com>
2021-07-18 00:20:52 +0200

    Update get_items.R

--- R/get_items.R
+++ R/get_items.R
@@ -10,11 +10,15 @@ get_items <- function(url, access_token){
   url <- urltools::param_set(url, key = "access-token", value = url_encode(access_token))
   result <- jsonlite::fromJSON(url)
   items <- result$items
+  total_count <- result$`_meta`$totalCount
   page_count <- result$`_meta`$pageCount
+  message(paste(total_count, "items in total, on", page_count, "pages"))
+  message("page 1")
   # links <- result$`_links`
   # page_count <- meta$pageCount
 
   if(page_count > 1){
+    message(paste("page",i))
     # for the remaining pages
     for(i in 2:page_count){
       Sys.sleep(2) # wait not to overload the server
@@ -24,7 +28,7 @@ get_items <- function(url, access_token){
       items <- rbind(items, items_new)
     }
   }
-  
+
   items %>% tibble()
 }
 
