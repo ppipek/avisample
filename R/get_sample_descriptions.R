8942de2f084857bb85a239e9efbf70d25804df3e
ppipek <ppipek@gmail.com>
2021-07-18 12:17:25 +0200

    Update get_sample_descriptions.R

--- R/get_sample_descriptions.R
+++ R/get_sample_descriptions.R
@@ -8,17 +8,25 @@
 #' @param country Pattern to filter sample descriptions by country
 #' @param tissue Pattern to filter sample descriptions by tissue type
 #' @param perPage Number of items on one page, maximum is 50 (and is set as default)
+#' @param strict If set to true, filter will not look for substrings but only for exact matches
 #' @importFrom urltools param_set url_encode
 #' @return A tibble
 #' @export
 
-get_sample_descriptions <- function (access_token, taxon_common = "",taxon_scientific = "", country = "", tissue = "", perPage = 50, limit = 1000){
+get_sample_descriptions <- function (access_token, taxon_common = "",taxon_scientific = "", country = "", tissue = "", perPage = 50, strict = F){
   url <- "https://avisample.net/api/sample"
-  url <- urltools::param_set(url, key = "filter[taxon_common][like]", value = urltools::url_encode(taxon_common))
-  url <- urltools::param_set(url, key = "filter[taxon_scientific][like]", value = urltools::url_encode(taxon_scientific))
-  url <- urltools::param_set(url, key = "filter[country][like]", value = urltools::url_encode(country))
-  url <- urltools::param_set(url, key = "filter[tissue][like]", value = urltools::url_encode(tissue))
+  if(strict == F){
+    url <- urltools::param_set(url, key = "filter[taxon_common][like]", value = urltools::url_encode(taxon_common))
+    url <- urltools::param_set(url, key = "filter[taxon_scientific][like]", value = urltools::url_encode(taxon_scientific))
+    url <- urltools::param_set(url, key = "filter[country][like]", value = urltools::url_encode(country))
+    url <- urltools::param_set(url, key = "filter[tissue][like]", value = urltools::url_encode(tissue))
+  }
+  else{
+    url <- urltools::param_set(url, key = "filter[taxon_common][eq]", value = urltools::url_encode(taxon_common))
+    url <- urltools::param_set(url, key = "filter[taxon_scientific][eq]", value = urltools::url_encode(taxon_scientific))
+    url <- urltools::param_set(url, key = "filter[country][eq]", value = urltools::url_encode(country))
+    url <- urltools::param_set(url, key = "filter[tissue][eq]", value = urltools::url_encode(tissue))
+  }
   url <- urltools::param_set(url, key = "per-page", value = perPage)
-  url <- urltools::param_set(url, key = "limit", value = limit)
   return(get_items(url, access_token))
 }
