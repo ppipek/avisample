521bc2f65e5e07e7b12a8648afc0a59cb751426e
ppipek <ppipek@gmail.com>
2021-07-18 23:12:56 +0200

    Update get_items.R

--- R/get_items.R
+++ R/get_items.R
@@ -16,12 +16,12 @@ get_items <- function(url, access_token){
     results = list()
     results[[1]] <- first_result$items
     message(paste(total_count, "items in total, on", page_count, "pages"))
-    message("Processing page 1")
+    message(paste("Processing page 1/",page_count, sep="")
 
     # for the remaining pages
     for(i in 2:page_count){
-      message(paste("Processing page",i))
       Sys.sleep(2) # wait not to overload the server
+      message(paste("Processing page ",i,"/",page_count, sep=""))
       url_new <- urltools::param_set(url, key = "page", value = i)
       result_new <- jsonlite::fromJSON(url_new)
       results[[i]] <- result_new$items
