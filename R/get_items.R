ecd6e5b4576ca154dde7af212be3ce63b5b910fc
ppipek <ppipek@gmail.com>
2021-07-18 11:44:13 +0200

    Initial commit

--- R/get_items.R
+++ R/get_items.R
@@ -8,28 +8,31 @@
 #' @return tibble
 get_items <- function(url, access_token){
   url <- urltools::param_set(url, key = "access-token", value = url_encode(access_token))
-  result <- jsonlite::fromJSON(url)
-  items <- result$items
-  total_count <- result$`_meta`$totalCount
-  page_count <- result$`_meta`$pageCount
-  message(paste(total_count, "items in total, on", page_count, "pages"))
-  message("page 1")
-  # links <- result$`_links`
-  # page_count <- meta$pageCount
+  first_result <- jsonlite::fromJSON(url)
+  total_count <- first_result$`_meta`$totalCount
+  page_count <- first_result$`_meta`$pageCount
 
   if(page_count > 1){
-    message(paste("page",i))
+    results = list()
+    results[[1]] <- first_result$items
+    message(paste(total_count, "items in total, on", page_count, "pages"))
+    message("Page 1")
+
     # for the remaining pages
     for(i in 2:page_count){
+      message(paste("Page",i))
       Sys.sleep(2) # wait not to overload the server
       url_new <- urltools::param_set(url, key = "page", value = i)
       result_new <- jsonlite::fromJSON(url_new)
-      items_new <- result_new$items
-      items <- rbind(items, items_new)
+      results[[i]] <- result_new$items
     }
+
+    results %>% bind_rows() %>% tibble()
   }
 
-  items %>% tibble()
+  else{
+    first_result$items %>% tibble()
+  }
 }
 
 
